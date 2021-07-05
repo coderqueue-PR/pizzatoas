b03833e950a61ead8d538bf0c931e86bc8d1c8f8
PR-coderqueue <72966339+coderqueue-PR@users.noreply.github.com>
2021-07-05 13:10:52 +0530

    initial Commit

---
+++ app/config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport')
+
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const user = require('../models/user')
+
+function init() {
+    passport.use(new LocalStrategy ({usernameField: 'email' }, async (email,password,done)=>{
+        //login
+        //Check if email exists
+      const user = await User.findOne({email: email})
+        if(!user){
+            return done(null, false , {message: 'No User with this email'})
+        }
+        bcrypt.compare(password , user.password).then(match =>{
+            if(match){
+                return done(null , user , {message: 'Logged in Successfully'} )
+            }
+            return done(null , false ,{message: 'wrong username or password'})
+        }).catch(err=>{
+            return done(null , false ,{message: 'Something went Wrong'})
+        })
+
+    }))
+
+    passport.serializeUser((user,done)=>{
+        done(null,user._id)
+    })
+
+    passport.deserializeUser((id, done)=>{
+        User.findById(id, (err,user)=>{
+            done(err,user)
+        })
+    })
+
+}
+
+module.exports = init
